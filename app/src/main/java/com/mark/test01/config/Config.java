dd064c572e17c188a294a0246fedeb4a1add9931
Markcz <qq784450151@gmail.com>
2017-12-11 21:58:08 +0800

    添加铃声设置

--- app/src/main/java/com/mark/test01/config/Config.java
+++ app/src/main/java/com/mark/test01/config/Config.java
@@ -1,5 +1,16 @@
 package com.mark.test01.config;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import java.io.File;
+
 /**
  * Created by Mark on 2017/11/27.
  */
@@ -68,5 +79,45 @@ public class Config {
         return cmd.split("\\*");
     }
 
+    public static void setMyRingtone(Context context, String path) {
+        File sdfile = new File(path);
+        String ab_path = sdfile.getAbsolutePath();
+        ContentValues values = new ContentValues();
+        Uri uri_query = MediaStore.Audio.Media.getContentUriForPath(ab_path);
+// 查询音乐文件在媒体库是否存在
+        Cursor cursor = context.getContentResolver().query(uri_query, null,
+                MediaStore.MediaColumns.DATA + "=?", new String[]{ab_path},
+                null);
+
+
+        if (cursor.moveToFirst() && cursor.getCount() > 0) {
+            String _id = cursor.getString(0);
+            Uri newUri = ContentUris.withAppendedId(uri_query,
+                    Long.valueOf(_id));
+            RingtoneManager.setActualDefaultRingtoneUri(context,
+                    RingtoneManager.TYPE_RINGTONE, newUri);
+            Toast.makeText(context, "设置电话铃声成功1！", Toast.LENGTH_SHORT).show();
+        } else {
+
+
+            values.put(MediaStore.MediaColumns.DATA, sdfile.getAbsolutePath());
+            values.put(MediaStore.MediaColumns.TITLE, sdfile.getName());
+            values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp3");
+            values.put(MediaStore.MediaColumns.SIZE, sdfile.length());
+
+            values.put(MediaStore.Audio.Media.IS_NOTIFICATION, true);
+
+//注意这一行，一定是只设置IS_NOTIFICATION这一个值。同理，如果设置通知音或闹铃也都只设置这一个值，只是在RingtoneManager.setActualDefaultRingtoneUri()这里区别是哪种铃声
+
+
+            Uri uri = MediaStore.Audio.Media.getContentUriForPath(sdfile
+                    .getAbsolutePath());
+            Uri newUri = context.getContentResolver().insert(uri, values);
+            RingtoneManager.setActualDefaultRingtoneUri(context,
+                    RingtoneManager.TYPE_RINGTONE, newUri);//这里设置不同的TYPE来区别铃声种类
+            Toast.makeText(context, "设置电话铃声成功2！", Toast.LENGTH_SHORT).show();
+            System.out.println("setMyRingtone()-----铃声");
+        }
+    }
 
 }
